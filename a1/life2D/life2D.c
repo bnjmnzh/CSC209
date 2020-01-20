5dc1f81317c50debdb6e5eaf2e259484899f666d
Benjamin Zhuo <zhuobenj@teach.cs.toronto.edu>
2020-01-20 03:37:15 -0500

    Finished part 2

--- a1/life2D/life2D.c
+++ a1/life2D/life2D.c
@@ -11,5 +11,20 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // TODO: Implement.
+    int num_rows = strtol(argv[1], NULL, 10);
+    int num_col = strtol(argv[2], NULL, 10);
+    int states = strtol(argv[3], NULL, 10);
+    int board[num_rows * num_col];
+    
+    int i = 0;
+    while(fscanf(stdin, "%d", &board[i]) == 1) {
+	    i++;
+    }
+
+    for (int i = 0; i < states; i++) {
+        print_state(board, num_rows, num_col);	    
+    	update_state(board, num_rows, num_col);
+    }
+
+    return 0;
 }
--- a1/life2D/life2D_helpers.c
+++ a1/life2D/life2D_helpers.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void print_state(int *board, int num_rows, int num_cols) {
     for (int i = 0; i < num_rows * num_cols; i++) {
         printf("%d", board[i]);
@@ -13,6 +12,51 @@ void print_state(int *board, int num_rows, int num_cols) {
 }
 
 void update_state(int *board, int num_rows, int num_cols) {
-    // TODO: Implement.
+    int total = num_rows * num_cols;
+    int new_board[total];
+    for (int i = 0; i < total; i++) {
+	// Edge cells don't change
+    	if ((0 <= i && i <= num_cols - 1) || (total - num_cols <= i && i <= total - 1) || (i % num_cols == 0)
+		||  ((i + 1) % num_cols == 0)) {
+	    ;
+	}
+	else {
+	    // west neighbour: i - 1
+	    // east neightbour: i + 1
+	    // north neighbour: i - num_col
+	    // south neighbour: i + num_col
+	    // north-east neighbour: i - num_col + 1
+	    // north-west neighbour: i - num_col - 1
+	    // south-east neighbour: i + num_col + 1
+	    // south-west neighbour: i + num_col - 1
+	    int count_alive = 0;
+	    int neighbours[8] = {i - 1, i + 1, i - num_cols, i + num_cols, i - num_cols + 1, i - num_cols - 1,
+		    i + num_cols + 1, i + num_cols - 1};
+	    for (int j = 0; j < 8; j++) {
+	    	if (board[neighbours[j]] == 1) { 
+		    count_alive++;
+		}	
+	    }
+	    if (board[i] == 1) {
+		if (count_alive < 2 || count_alive > 3) {
+		    new_board[i] = 0;
+		} else {
+		    new_board[i] = 1;
+		}
+	    } else if (board[i] == 0) {
+		if (count_alive == 2 || count_alive == 3) {
+		    new_board[i] = 1;
+		} else {
+		    new_board[i] = 0;
+		}
+	    }
+	}
+    }
+    for (int i = 0; i < total; i++) {
+	if (!((0 <= i && i <= num_cols - 1) || (total - num_cols <= i && i <= total - 1) || (i % num_cols == 0)
+        	||  ((i + 1) % num_cols == 0))) {
+    	   board[i] = new_board[i];
+	}
+    }
     return;
 }
