7624af5d74b74f6ff20531ecd54c26a3b1097d02
Benjamin Zhuo <zhuobenj@teach.cs.toronto.edu>
2020-02-09 00:41:33 -0500

    implemented base case

--- a2/ftree.c
+++ a2/ftree.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
 // Add your system includes here.
 
 #include "ftree.h"
 
-
+struct TreeNode *ftree_helper(const char *fname, char *path);
 /*
  * Returns the FTree rooted at the path fname.
  *
@@ -20,8 +26,78 @@ struct TreeNode *generate_ftree(const char *fname) {
     // helper function, the path would be "", since fname is the root
     // of the FTree.  For files at other depths, the path would be the
     // file path from the root to that file.
+    
+    char path[50] = "";
+    struct TreeNode *tree_ptr = ftree_helper(fname, path);
+    if (tree_ptr == NULL) {
+        return NULL;
+    }
+    return tree_ptr;
+}
 
-    return NULL;
+struct TreeNode *ftree_helper(const char *fname, char *path) {
+    struct TreeNode* tree_ptr = malloc(sizeof(struct TreeNode));
+   
+    strcat(path, fname);
+    struct stat stat_buf; 
+
+    if (lstat(path, &stat_buf) == -1) {
+    	fprintf(stderr, "The path (%s) does not point to an existing entry!\n", path);
+    }
+
+    tree_ptr->permissions = stat_buf.st_mode & 0777;
+    
+    tree_ptr->fname = (char *)fname;
+    tree_ptr->contents = NULL;
+    tree_ptr->next = NULL; 
+
+    if (S_ISREG(stat_buf.st_mode)) { // Base case
+        // Code to execute for regular file
+	printf("encounted regular file %s\n", path);
+	strcpy(&(tree_ptr->type), "-");
+    }
+
+    else if (S_ISLNK(stat_buf.st_mode)) { // Base case
+        // Code to execute for symbolic link 
+	printf("encountered symbolic link %s\n", path);
+        strcpy(&(tree_ptr->type), "l");
+    }
+
+    else if (S_ISDIR(stat_buf.st_mode)) { // Recursive case
+    	// Code to execute for a directory
+	strcpy(&(tree_ptr->type), "d");
+	
+	DIR *d_ptr = opendir(path);
+	if (d_ptr == NULL) {
+	    fprintf(stderr, "Error on opening %s\n", path);
+	    return NULL;
+	}
+	struct dirent *entry_ptr;
+	entry_ptr = readdir(d_ptr);
+	
+	tree_ptr->contents = malloc(sizeof(struct TreeNode *));
+	tree_ptr->next = malloc(sizeof(struct TreeNode *));
+	
+	char temp_path[50];
+	strcpy(temp_path, path);
+	strcat(temp_path, "/");
+	
+	int len = strlen(temp_path);
+
+	while (entry_ptr != NULL) {
+	    if (entry_ptr->d_name[0] != '.') {
+		tree_ptr->next = ftree_helper(entry_ptr->d_name, temp_path);
+	        temp_path[len] = '\0';
+	    }
+	    entry_ptr = readdir(d_ptr);
+	}
+
+	if (closedir(d_ptr) == -1) {
+	    fprintf(stderr, "Error on closing %s\n", path);
+	    return NULL;
+	}
+    }
+    return tree_ptr;
 }
 
 
@@ -41,6 +117,11 @@ void print_ftree(struct TreeNode *root) {
     printf("%*s", depth * 2, "");
 
     // Your implementation here.
+    if (root->contents == NULL) { // Regular file or symbolic link
+	printf("%s (%c%o)\n", root->fname, root->type, root->permissions);
+    } else {
+        printf("===== %s (%c%o) =====\n", root->fname, root->type, root->permissions);
+    }
 }
 
 
@@ -51,5 +132,8 @@ void print_ftree(struct TreeNode *root) {
 void deallocate_ftree (struct TreeNode *node) {
    
    // Your implementation here.
-
+   if (node->contents != NULL) {
+	return;
+   }
+   free(node);
 }
