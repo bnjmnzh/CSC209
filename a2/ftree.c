6094a067a0b027ec83dbb530b31d7cbe328cac5d
Benjamin Zhuo <zhuobenj@teach.cs.toronto.edu>
2020-02-11 14:10:05 -0500

    Fixed comment error

--- a2/ftree.c
+++ a2/ftree.c
@@ -133,7 +133,7 @@ void print_ftree(struct TreeNode *root) {
     // Here's a trick for remembering what depth (in the tree) you're at
     // and printing 2 * that many spaces at the beginning of the line.
     static int depth = 0;
-//    printf("%*s", depth * 2, "");
+    printf("%*s", depth * 2, "");
 
     if (root->type != 'd') { // Regular file or symbolic link
 	printf("%s (%c%o)\n", root->fname, root->type, root->permissions);
