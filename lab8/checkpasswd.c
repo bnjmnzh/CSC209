5e08e3ee7f1f65bf1a89f05442eb838b5014b03f
Benjamin Zhuo <zhuobenj@teach.cs.toronto.edu>
2020-03-03 21:34:38 -0500

    Finished checkpasswd.c

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 
 #define MAXLINE 256
+#define MAXPASSWORD 10
 
 #define SUCCESS "Password verified\n"
 #define INVALID "Invalid password\n"
@@ -30,7 +31,65 @@ int main(void) {
       exit(1);
   }
   
-  // TODO
+  int fd[2];
+
+  if (pipe(fd) == -1) {
+      perror("pipe");
+      exit(1);
+  }
+
+  int r = fork();
+
+  if (r > 0) {
+      if (close(fd[0]) == -1) {
+          perror("close");
+	  exit(1);
+      }
+      if (write(fd[1], user_id, MAXPASSWORD) == -1) {
+          perror("write");
+	  exit(1);
+      }
+      if (write(fd[1], password, MAXPASSWORD) == -1) {
+          perror("write");
+	  exit(1);
+      }
+      if (close(fd[1])== -1) {
+          perror("close");
+	  exit(1);
+      }
+
+      int status;
+      if (wait(&status) == -1) {
+          perror("wait");
+	  exit(1);
+      }
+      if (WIFEXITED(status)) {
+          if (WEXITSTATUS(status) == 0) {
+	      printf(SUCCESS);
+	  } else if (WEXITSTATUS(status) == 2) {
+	      printf(INVALID);    
+	  } else if (WEXITSTATUS(status) == 3) {
+	      printf(NO_USER);
+	  } else {
+	      // status = 1 
+	      exit(1);
+	  }
+      }
+  } else if (r == 0) {
+      if (close(fd[1]) == -1) {
+          perror("close");
+      }
+      if (dup2(fd[0], STDIN_FILENO) == -1 ) {
+          perror("dup2");
+	  exit(1);
+      }
+      if (close(fd[0]) == -1) {
+          perror("close");
+	  exit(1);
+      }
+
+      execl("./validate", "validate", NULL);
+  }
 
   return 0;
 }
