619503690efcc39def07af09c6d8827faf744b80
Benjamin Zhuo <zhuobenj@teach.cs.toronto.edu>
2020-03-28 05:25:49 -0400

    Finished task 3

--- lab11/chat_client.c
+++ lab11/chat_client.c
@@ -37,6 +37,12 @@ int main(void) {
         exit(1);
     }
 
+    int max_fd = sock_fd;
+    fd_set all_fds;
+    FD_ZERO(&all_fds);
+    FD_SET(sock_fd, &all_fds);
+    FD_SET(STDIN_FILENO, &all_fds);
+
     // Get the user to provide a name.
     char buf[2 * BUF_SIZE + 1];            // 2x to allow for usernames
     printf("Please enter a username: ");
@@ -48,27 +54,37 @@ int main(void) {
     // Read input from the user, send it to the server, and then accept the
     // echo that returns. Exit when stdin is closed.
     while (1) {
-        int num_read = read(STDIN_FILENO, buf, BUF_SIZE);
-        if (num_read == 0) {
-            break;
-        }
-        buf[num_read] = '\0';
+	fd_set listen_fds = all_fds;
+	int nready = select(max_fd + 1, &listen_fds, NULL, NULL, NULL);
+	if (nready == -1) {
+	    perror("client: select");
+	    exit(1);
+	}
 
-        /* 
-         * We should really send "\r\n" too, so the server can identify partial 
-         * reads, but you are not required to handle partial reads in this lab.
-         */
+	if (FD_ISSET(STDIN_FILENO, &listen_fds)) {
+       	    int num_read = read(STDIN_FILENO, buf, BUF_SIZE);
+            if (num_read == 0) {
+            	break;
+            }
+            buf[num_read] = '\0';
 
-        int num_written = write(sock_fd, buf, num_read);
-        if (num_written != num_read) {
-            perror("client: write");
-            close(sock_fd);
-            exit(1);
-        }
+            /* 
+            * We should really send "\r\n" too, so the server can identify partial 
+            * reads, but you are not required to handle partial reads in this lab.
+             */
 
-        num_read = read(sock_fd, buf, BUF_SIZE);
-        buf[num_read] = '\0';
-        printf("Received from server: %s", buf);
+            int num_written = write(sock_fd, buf, num_read);
+            if (num_written != num_read) {
+                perror("client: write");
+                close(sock_fd);
+                exit(1);
+            }
+	}
+	if (FD_ISSET(sock_fd, &listen_fds)) {
+            num_read = read(sock_fd, buf, BUF_SIZE);
+            buf[num_read] = '\0';
+            printf("%s", buf);
+	}
     }
 
     close(sock_fd);
